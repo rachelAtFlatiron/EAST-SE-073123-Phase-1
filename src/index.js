f15cb0113ac7930f56dc0c79cf51507dc1787517
Rachel Katz <rachel.katz@faltironschool.com>
2023-07-31 16:42:41 -0400

    demo done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 //Data 
+let newString = "string1" + "string2"
+
 const inventory = [
   {
     id: 1,
@@ -67,28 +69,107 @@ const inventory = [
 
 //✅ 1. Create hello world using REGULAR functions
 function helloWorld(){
-  return
+  console.log("hello world!")
+  //return "Hello World!"
 }
+
+//make sure to return from helloWorld, otherwise undefinedTest may save as undefined
+let undefinedTest = helloWorld() 
+console.log(undefinedTest)
+
+
 //✅ 2. For Easley's bookstore, create formatPrice(price)
+function formatPrice(price) { //ex. price = 3.333333
+  //make string in the format $x.xx
+  //rounded properly , limit to 2 decimal spaces
+  let fixedPrice = price.toFixed(2)
+  //add $
+  let dollarPrice = `$${fixedPrice}`
+  //return it (such that formatPrice will evaluate to a value)
+  return `$${price.toFixed(2)}`
+}
+
 
 //✅ 3. Make an arrow function version of formatPrice
+//curly braces if explicit return, no curlies if implicit return
+const arrowFormatPrice = (price) => {
+  let fixedPrice = price.toFixed(2)
+  //add $
+  let dollarPrice = `$${fixedPrice}`
+  //return it (such that formatPrice will evaluate to a value)
+  return `$${dollarPrice}`
+}
 
 //✅ 4. WE DO: create a blurb() function that accepts a book as an argument and logs a message in the following format:
 //'Eloquent JavaScript: A Modern Introduction to Programming by Marjin Haverbeke is on sale for $10.00'
+/*
+  book = {
+    id: 1,
+    title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+    author: 'Marjin Haverbeke',
+    price: 10.00,
+    reviews: [{userID: 1, content:'Good book, but not great for new coders'}],
+    inventory: 10,
+    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg'
+  }
+*/
+const blurb = (book) => {
+  //'Eloquent JavaScript: A Modern Introduction to Programming by Marjin Haverbeke is on sale for $10.00'
+  let title = book.title 
+  let price = book.price 
+  let formattedPrice = formatPrice(price) //formatPrice(price) spit out $10.00
+  return `${title} is on sale for ${formattedPrice}`
+}
 
 //✅ 5. Call formatPrice on an array of prices
 
 //✅ 5a. Create an array
+let priceList = [2.2342, 15.235, 9283.255]
+formatPrice(3.3333)
+formatPrice(2.3824)
+formatPrice(10.283)
+
+formatPrice(price[0])
+formatPrice(price[1])
+formatPrice(price[2])
 
 //✅ 5b. Use a for loop to iterate over prices
+//a loop that runs for each thing in priceList
+//start 0, end at the end of price list, increment by 1
+console.log(priceList.length)
+for(let i = 0; i < priceList.length; i++){ // 3
+  console.log(i) 
+  console.log(priceList[i]) 
+  console.log(formatPrice(priceList[i]))
 
+}
+
+//console.log(formatPrice(2.3423 + 235.300))
 //✅ 5c. Use .forEach to iterate over prices
+//array.forEach(el => cbFunction)
+let forEachReturnValue = priceList.forEach(price => console.log(formatPrice(price)))
 
 //✅ 5d. Use .map to iterate over prices
+//price represents the current element .map is at 
+//if formatPrice didn't return anything, we'd get an array of undefineds
+//because formatPrice didn't spit out anything to replace the current element with
+//array.map(el => cbFunction)
+let mapPrice = priceList.map(price => formatPrice(price))
+
 
 //✅ 5e. using .map, for each book in inventory, return blurb(book)
 //output: ['Eloquent JavaScript: A Modern Introduction to Programming is on sale for $10.00', ...]
+let blurbs = inventory.map(book => blurb(book))
 
 //✅ 6. Create a version of myMap that uses a for loop to mimic .map
 //input: array, callback function
 //output: a new array
+
+//Here is one solution for the above:
+function myMap(array, cb){
+  const newArray = []
+  for(let i = 0; i < array.length; i++){
+    newArray.push(cb(array[i]))
+  }
+  return newArray;
+}
