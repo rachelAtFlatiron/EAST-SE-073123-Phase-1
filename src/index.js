96611e148e9a63d087cf078978fef525fec0575b
Rachel Katz <rachel.katz@faltironschool.com>
2023-08-02 15:26:37 -0400

    deliverable 1 and 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 /* constants */
 const bookList = document.querySelector("#book-list");
-
+const newBookBtn = document.querySelector("#toggleForm");
+const bookForm = document.querySelector('#book-form')
 /**
  * 
  * Renders a book card to DOM given a book object
  */
+
+//newBookBtn.addEventListener('click', (e) => console.log(e))
+
 function renderBook(book) {
 	const li = document.createElement("li");
 	li.className = "card";
@@ -19,11 +23,18 @@ function renderBook(book) {
 	priceNode.textContent = formatPrice(book.price);
 	imgNode.src = book.imageUrl;
 	deleteBtn.textContent = "Delete";
+	deleteBtn.className = book.title
 	//✅ 1. on delete button click, remove card from DOM
+
 	//✅ 1a. attach eventListener
 	//✅ 1b. include callback function to remove card instance
-
-	//✅ 1c. define cb outside of renderBook
+	deleteBtn.addEventListener('click', (e) => {
+		li.remove()
+	})
+	//brand new deleteBtn and brand new li get created every time renderBook is executed
+	//see .forEach at bottom of page
+	//console.log(deleteBtn, li)
+	//console.log()
 
 	bookList.append(li);
 	li.append(titleNode);
@@ -31,18 +42,37 @@ function renderBook(book) {
 	li.append(priceNode);
 	li.append(imgNode);
 	li.append(deleteBtn);
+
+	//✅ 1c. define cb outside of renderBook
 }
 
 //✅ 2. add a submit event listener to the form
+
 //✅ 2a. save the form node as a const
 //✅ 2b. write a testing function to fill in the values of the form
+
 //✅ 2c. invoke the fill in function
 //✅ 2d. create the event listener 
+bookForm.addEventListener('submit', (e) => {
+	
+	e.preventDefault() //to stop page from refreshing (redirecting)
 
 	//✅ 2e. create the new book and add to DOM
+	//create new book object that contains information from form
+	//AND aligns with data structure for a book found in data.js
+	let newBook = {
+		title: e.target.title.value,
+		author: e.target.author.value,
+		price: e.target.price.value,
+		imageUrl: e.target.imageUrl.value,
+		inventory: e.target.inventory.value
+	}
+	//reuse renderBook logic to put card with newBook information in DOM
+	renderBook(newBook)
+})
 
 
-//✅  3. recap - show the form when you click on the "add new book" button
+//✅ 3. recap - show the form when you click on the "add new book" button
 //✅ 3a. save the button in a variable
 //✅ 3b. add the event listener
 	//✅ 3c. hide/show the form
@@ -77,4 +107,5 @@ function renderFooter(bookStore) {
 /* invoke functions on DOM content loaded */
 renderHeader(bookStore);
 renderFooter(bookStore);
-bookStore.inventory.forEach(book => renderBook(book));
+//
+bookStore.inventory.forEach((book) => renderBook(book));
