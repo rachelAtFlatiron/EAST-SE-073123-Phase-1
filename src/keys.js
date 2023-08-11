5dcd0249cfc7ee53d056495e42dfbaf48f150544
Rachel Katz <rachel.katz@flatironschool.com>
2023-08-11 11:23:33 -0400

    deleted keys

--- src/keys.js
+++
@@ -1,9 +0,0 @@
-//✅ 2. create keys.js file to hold API key for Google books
-
-//✅ 2a. save API_KEY
-const key = 'AIzaSyBOgjh7jtr79Af4hif6xzKf-lDLUUN5znI'
-
-//✅ 2b. create .gitignore to ignore keys.js
-
-//✅ 3. use google books api to get information in browser
-//✅ 3a. test getting information from the API in browser
